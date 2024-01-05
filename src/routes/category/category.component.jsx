acd396cb20b028b7154d1ba556cb44e558e1fe7c
flvSantos15 <flvSantos300@gmail.com>
2024-01-05 07:59:41 -0300

    chore: useMutatino

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState, useEffect, Fragment } from 'react'
 import { useParams } from 'react-router-dom'
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
 
 import { ProductCard } from '../../components/product-card/product-card.component'
 import { Spinner } from '../../components/spinner/spinner.component'
@@ -24,6 +24,21 @@ const GET_CATEGORY = gql`
   }
 `
 
+// const SET_CATEGORY = gql`
+//   mutation ($category: Category) {
+//     addCategory(category: $category) {
+//       id
+//       title
+//       items {
+//         id
+//         name
+//         price
+//         imageUrl
+//       }
+//     }
+//   }
+// `
+
 const Category = () => {
   const { category } = useParams()
   const { loading, error, data } = useQuery(GET_CATEGORY, {
@@ -31,6 +46,10 @@ const Category = () => {
       title: category
     }
   })
+
+  // const [addCategory, { loading, error, data }] = useMutation(SET_CATEGORY)
+
+  // addCategory({ variables: { category: categoryObject } })
 
   const [products, setProducts] = useState([])
 
